e0d722b8bd11f5bd0f61151b892dd0269c069423
bfoing <40759640+bfoing@users.noreply.github.com>
2020-09-18 01:24:31 +0100

    chore: add createKey and importKey helpers

--- src/secretarium.keymanager.ts
+++ src/secretarium.keymanager.ts
@@ -1,36 +1,15 @@
 import * as Utils from './secretarium.utils';
 import { ErrorMessage, ErrorCodes } from './secretarium.constant';
 
-export interface KeyBase {
-    name: string;
-    version: number;
-}
-export interface ExportedKey extends KeyBase {
+export interface ExportedKey {
     publicKey: JsonWebKey; // need to use raw (pkcs8) as soon as firefox fixes bug 1133698
     privateKey: JsonWebKey; // need to use raw (pkcs8) as soon as firefox fixes bug 1133698
 }
-export interface ExportedKeyEncrypted extends KeyBase {
-    iv: string;
-    salt: string;
-    data: string;
-}
 
-export class Key implements KeyBase {
-
-    name = '';
-    version = 0;
+export class Key {
     cryptoKey: CryptoKeyPair | null = null;
     publicKeyRaw: Uint8Array | null = null;
-
-    save = false;
-    imported = false;
-    encrypted = false;
-    isNew = false;
-
     exportableKey: ExportedKey | null = null;
-    exportUrl = '';
-    exportableKeyEncrypted: ExportedKeyEncrypted | null = null;
-    exportUrlEncrypted = '';
 
     async setCryptoKey(publicKey: CryptoKey, privateKey: CryptoKey): Promise<void> {
         this.cryptoKey = { publicKey: publicKey, privateKey: privateKey };
@@ -45,5 +24,23 @@ export class Key implements KeyBase {
     getPublicKeyHex(delimiter = ''): string {
         if (!this.publicKeyRaw) throw new Error(ErrorMessage[ErrorCodes.EKEYISENC]);
         return Utils.toHex(this.publicKeyRaw, delimiter);
+    }
+
+    static async createKey(): Promise<Key> {
+        const cryptoKey = await window.crypto.subtle.generateKey({ name: 'ECDSA', namedCurve: 'P-256' }, true, ['sign', 'verify']);
+        const key = new Key();
+        key.setCryptoKey(cryptoKey.publicKey, cryptoKey.privateKey);
+        key.exportableKey = {
+            publicKey: await window.crypto.subtle.exportKey('jwk', cryptoKey.publicKey),
+            privateKey: await window.crypto.subtle.exportKey('jwk', cryptoKey.privateKey)
+        };
+        return key;
+    }
+
+    static async importKey(exported: ExportedKey): Promise<Key> {
+        const key = new Key();
+        key.exportableKey = exported;
+        await key.setCryptoKeyFromJwk(key.exportableKey.publicKey, key.exportableKey.privateKey);
+        return key;
     }
 }
