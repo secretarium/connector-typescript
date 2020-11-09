7228884eb1fa32fb343271de54dd93113d1aed9e
bfoing <40759640+bfoing@users.noreply.github.com>
2020-11-09 19:46:23 +0000

    chore: add seal unseal function to Secretarium keys

--- src/secretarium.key.ts
+++ src/secretarium.key.ts
@@ -5,6 +5,13 @@ import crypto from './msrcrypto';
 export interface ExportedKey {
     publicKey: JsonWebKey; // need to use raw (pkcs8) as soon as firefox fixes bug 1133698
     privateKey: JsonWebKey; // need to use raw (pkcs8) as soon as firefox fixes bug 1133698
+    version: number;
+}
+export interface ExportedKeyEncrypted {
+    iv: string;
+    salt: string;
+    data: string;
+    version: number;
 }
 
 export class Key {
@@ -12,6 +19,8 @@ export class Key {
     cryptoKey: CryptoKeyPair | null = null;
     publicKeyRaw: Uint8Array | null = null;
     exportableKey: ExportedKey | null = null;
+    exportableKeyEncrypted: ExportedKeyEncrypted | null = null;
+    version = 1;
 
     async setCryptoKey(publicKey: CryptoKey, privateKey: CryptoKey): Promise<void> {
         this.cryptoKey = { publicKey: publicKey, privateKey: privateKey };
@@ -28,11 +37,73 @@ export class Key {
         return Utils.toHex(this.publicKeyRaw, delimiter);
     }
 
+    async seal(pwd: string): Promise<Key> {
+        if (!this.cryptoKey) throw new Error(ErrorMessage[ErrorCodes.EKEYISENC]);
+
+        const salt = Utils.getRandomBytes(32),
+            iv = Utils.getRandomBytes(12),
+            weakPwd = Utils.encode(pwd),
+            strongPwd = await Utils.hash(Utils.concatBytes(salt, weakPwd)),
+            key = await window.crypto.subtle.importKey('raw', strongPwd, 'AES-GCM', false, ['encrypt', 'decrypt']),
+            json = JSON.stringify({ publicKey: this.exportableKey?.publicKey, privateKey: this.exportableKey?.privateKey }),
+            data = Utils.encode(json),
+            encrypted = new Uint8Array(await window.crypto.subtle.encrypt({ name: 'AES-GCM', iv: iv, tagLength: 128 }, key, data));
+        this.exportableKeyEncrypted = {
+            version: this.version, iv: Utils.toBase64(iv),
+            salt: Utils.toBase64(salt), data: Utils.toBase64(encrypted)
+        };
+        return this;
+    }
+
+    async unseal(pwd: string): Promise<Key> {
+        if (this.cryptoKey) return this;
+        if (!this.exportableKeyEncrypted) throw 'Key has not been imported';
+
+        const iv = Utils.encode(this.exportableKeyEncrypted.iv),
+            salt = Utils.encode(this.exportableKeyEncrypted.salt),
+            encrypted = Utils.encode(this.exportableKeyEncrypted.data),
+            weakpwd = Utils.encode(pwd),
+            strongPwd = await Utils.hash(Utils.concatBytes(salt, weakpwd)),
+            key = await window.crypto.subtle.importKey('raw', strongPwd, 'AES-GCM', false, ['encrypt', 'decrypt']);
+        let decrypted: Uint8Array;
+        try {
+            decrypted = new Uint8Array(await window.crypto.subtle.decrypt({ name: 'AES-GCM', iv: iv, tagLength: 128 }, key, encrypted));
+        }
+        catch (e) {
+            throw new Error(ErrorMessage[ErrorCodes.EINPASSWD]);
+        }
+
+        try {
+            if (this.version == 0) { // retro compat
+                const publicKey = await window.crypto.subtle.importKey('raw', decrypted.subarray(0, 65), { name: 'ECDSA', namedCurve: 'P-256' }, true, ['verify']);
+                const privateKey = await window.crypto.subtle.importKey('pkcs8', decrypted.subarray(65), { name: 'ECDSA', namedCurve: 'P-256' }, true, ['sign']);
+                await this.setCryptoKey(publicKey, privateKey);
+                this.exportableKey = {
+                    version: 1,
+                    publicKey: await window.crypto.subtle.exportKey('jwk', publicKey),
+                    privateKey: await window.crypto.subtle.exportKey('jwk', privateKey)
+                };
+                await this.seal(pwd); // re-encrypt will migrate the encrypted exportable to the latest version
+            }
+            else {
+                const d = JSON.parse(Utils.decode(decrypted));
+                await this.setCryptoKeyFromJwk(d.publicKey, d.privateKey);
+                this.exportableKey = { version: 1, publicKey: d.publicKey, privateKey: d.privateKey };
+            }
+
+        }
+        catch (e) {
+            throw new Error(ErrorMessage[ErrorCodes.EINVKFORM]);
+        }
+
+        return this;
+    }
+
     static async createKey(): Promise<Key> {
         const cryptoKey = await crypto.subtle?.generateKey({ name: 'ECDSA', namedCurve: 'P-256' }, true, ['sign', 'verify']);
         const key = new Key();
         await key.setCryptoKey(cryptoKey.publicKey, cryptoKey.privateKey);
-        key.exportableKey = {
+        key.exportableKey = { version: 1,
             publicKey: await crypto.subtle?.exportKey('jwk', cryptoKey.publicKey),
             privateKey: await crypto.subtle?.exportKey('jwk', cryptoKey.privateKey)
         };
