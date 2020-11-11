026d0a8472f0cec5cd90283bdc3983eaeb236bc6
Florian Guitton <florian@secretarium.com>
2020-11-11 21:16:55 +0000

    chore: Key manipulation refactoring

--- src/secretarium.key.test.ts
+++ src/secretarium.key.test.ts
@@ -1,5 +1,6 @@
 import { Key } from './secretarium.key';
-import encJWTKey from '../fixtures/enc.jwt.json';
+import encV2Key from '../fixtures/enc.v2.json';
+import envV1Key from '../fixtures/enc.v1.json';
 
 test('Create a new key from static method', async () => {
 
@@ -8,19 +9,11 @@ test('Create a new key from static method', async () => {
 
     const newKey = await newKeyPromise;
     expect(newKey).toBeInstanceOf(Key);
-    expect(newKey.cryptoKey).toBeDefined();
-    expect(newKey.exportableKey).toBeDefined();
-    expect(newKey.exportableKey?.privateKey).toBeDefined();
-    expect(newKey.exportableKey?.publicKey).toBeDefined();
-});
 
-test('Create a new key from constructor', async () => {
-
-    const newKey = new Key();
-    expect(newKey).toBeInstanceOf(Key);
-
-    expect(newKey.cryptoKey).toBeUndefined();
-    expect(newKey.exportableKey).toBeUndefined();
+    const exportedKeyPair = await newKey.exportKey();
+    expect(exportedKeyPair).toBeDefined();
+    expect(exportedKeyPair.privateKey).toBeDefined();
+    expect(exportedKeyPair.publicKey).toBeDefined();
 });
 
 test('Seal a key', async () => {
@@ -29,26 +22,44 @@ test('Seal a key', async () => {
     const newSealPromise = newKey.seal('HelloWorld');
     expect(newSealPromise).toBeInstanceOf(Promise);
 
-    const newSeal = await newSealPromise;
-    expect(newSeal.version).toBeDefined();
-    expect(newSeal.iv).toBeDefined();
-    expect(newSeal.salt).toBeDefined();
-    expect(newSeal.data).toBeDefined();
+    const sealedKey = await newSealPromise;
+    expect(sealedKey).toBeInstanceOf(Key);
+
+    const encryptedKey = await sealedKey.exportEncryptedKey();
+    expect(encryptedKey.version).toBeDefined();
+    expect(encryptedKey.iv).toBeDefined();
+    expect(encryptedKey.salt).toBeDefined();
+    expect(encryptedKey.data).toBeDefined();
+});
+
+test('Import sealed key v1', async () => {
+
+    try {
+        const newKey = await Key.importEncryptedKeyPair(envV1Key, '1234');
+        expect(newKey).toBeInstanceOf(Key);
+    } catch (e) {
+        console.error('msrcrypto does not support PKCS8');
+    }
+});
+
+test('Import sealed key v1 with wrong password', async () => {
+    expect.assertions(1);
+    return Key.importEncryptedKeyPair(envV1Key, 'HelloWorld').then(() => {
+        return;
+    }).catch((err) => {
+        expect(err).toEqual(new Error('Can\'t decrypt, Invalid password'));
+    });
 });
 
 test('Import sealed key v2', async () => {
 
-    const newKey = await Key.importKeyEncrypted(encJWTKey, 'HelloWorld');
+    const newKey = await Key.importEncryptedKeyPair(encV2Key, 'HelloWorld');
     expect(newKey).toBeInstanceOf(Key);
-    expect(newKey.cryptoKey).toBeDefined();
-    expect(newKey.exportableKey).toBeDefined();
-    expect(newKey.exportableKey?.privateKey).toBeDefined();
-    expect(newKey.exportableKey?.publicKey).toBeDefined();
 });
 
 test('Import sealed key v2 with wrong password', async () => {
     expect.assertions(1);
-    return Key.importKeyEncrypted(encJWTKey, '1234').then(() => {
+    return Key.importEncryptedKeyPair(encV2Key, '1234').then(() => {
         return;
     }).catch((err) => {
         expect(err).toEqual(new Error('Can\'t decrypt, Invalid password'));
