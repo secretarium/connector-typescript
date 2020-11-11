3816b12f4892a0e58033615f1b8a87ca27a746cd
Florian Guitton <florian@secretarium.com>
2020-11-11 21:16:36 +0000

    chore: Modify exported methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-export * from './secretarium';
+import * as Utils from './secretarium.utils';
+import * as Constants from './secretarium.constant';
+import * as TC from './secretarium.connector';
+
+export type Query = TC.Query;
+export type Transaction = TC.Transaction;
+
+export {
+    Utils,
+    Constants
+};
+
+export {
+    Key
+} from './secretarium.key';
+
+
+export {
+    SCP
+} from './secretarium.connector';
