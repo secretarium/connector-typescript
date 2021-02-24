91d50d2d8f0bcf0299f3e0a596fc6333c397036e
Florian Guitton <florian@secretarium.com>
2021-02-24 17:35:17 +0000

    chore: Fix linting issue

--- src/secretarium.crypto.ts
+++ src/secretarium.crypto.ts
@@ -3,6 +3,6 @@ import msrCrypto from '../vendor/msrCrypto';
 const selectCrypto = () => {
     const finalCrypto = window.crypto || msrCrypto;
     return finalCrypto;
-}
+};
 
 export default selectCrypto();
