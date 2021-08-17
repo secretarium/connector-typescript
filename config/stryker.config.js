b42b33a2d7accf1a4923a49240dbe9b3412735db
Florian Guitton <florian@secretarium.com>
2021-08-17 18:11:30 +0100

    chore: Updating package dependencies

--- config/stryker.config.js
+++ config/stryker.config.js
@@ -1,20 +1,18 @@
-module.exports = (config) => {
-    config.set({
-        mutate: [
-            'src/**/*.[jt]s?(x)',
-            '!src/**/?(*.)+(spec|test).[jt]s?(x)'
-        ],
-        packageManager: 'yarn',
-        reporters: ['html', 'clear-text', 'progress', 'dashboard'],
-        testRunner: 'jest',
-        coverageAnalysis: 'off',
-        maxConcurrentTestRunners: 4,
-        timeoutMS: 30000,
-        jest: {
-            enableFindRelatedTests: false
-        },
-        dashboard: {
-            reportType: 'full'
-        }
-    });
+module.exports = {
+    mutate: [
+        'src/**/*.[jt]s?(x)',
+        '!src/**/?(*.)+(spec|test).[jt]s?(x)'
+    ],
+    packageManager: 'yarn',
+    reporters: ['html', 'clear-text', 'progress', 'dashboard'],
+    testRunner: 'jest',
+    coverageAnalysis: 'off',
+    concurrency: 4,
+    timeoutMS: 30000,
+    jest: {
+        enableFindRelatedTests: false
+    },
+    dashboard: {
+        reportType: 'full'
+    }
 };
